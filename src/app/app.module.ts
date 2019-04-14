b9cd76e82f46af75cbfaa1ac7b87b7f27b456984
houshuai <houshuai>
2019-04-14 20:59:59 +0800

    待开发 价格页面  和  跨域问题

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,64 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { RouterModule } from '@angular/router';
-import { AppRoutingModule } from './app.routing';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {RouterModule} from '@angular/router';
+import {AppRoutingModule} from './app.routing';
 
-import { AppComponent } from './app.component';
-import { SignupComponent } from './signup/signup.component';
-import { LandingComponent } from './landing/landing.component';
-import { ProfileComponent } from './profile/profile.component';
-import { NavbarComponent } from './shared/navbar/navbar.component';
-import { FooterComponent } from './shared/footer/footer.component';
+import {AppComponent} from './app.component';
+import {SignupComponent} from './signup/signup.component';
+import {LandingComponent} from './landing/landing.component';
+import {ProfileComponent} from './profile/profile.component';
+import {NavbarComponent} from './shared/navbar/navbar.component';
+import {FooterComponent} from './shared/footer/footer.component';
 
-import { LoginComponent } from './login/login.component';
-import { PriceComponent } from './price/price.component';
-import { DocumentComponent } from './document/document.component';
+import {LoginComponent} from './login/login.component';
+import {PriceComponent} from './price/price.component';
+import {DocumentComponent} from './document/document.component';
+import {MarkdownModule, MarkedOptions} from "ngx-markdown";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {TreeModule} from "ng2-tree";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SignupComponent,
-    LandingComponent,
-    ProfileComponent,
-    NavbarComponent,
-    FooterComponent,
-    LoginComponent,
-    PriceComponent,
-    DocumentComponent
-  ],
-  imports: [
-    BrowserModule,
-    NgbModule.forRoot(),
-    FormsModule,
-    RouterModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        SignupComponent,
+        LandingComponent,
+        ProfileComponent,
+        NavbarComponent,
+        FooterComponent,
+        LoginComponent,
+        PriceComponent,
+        DocumentComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        TreeModule,
+        MarkdownModule.forRoot(
+            {
+                loader: HttpClient,
+                markedOptions: {
+                    provide: MarkedOptions,
+                    useValue: {
+                        gfm: true,
+                        tables: true,
+                        breaks: false,
+                        pedantic: false,
+                        sanitize: false,
+                        smartLists: true,
+                        smartypants: false,
+                    }
+                }
+            }
+        ),
+        NgbModule.forRoot(),
+        FormsModule,
+        RouterModule,
+        AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/document/document.component.ts
+++ src/app/document/document.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NodeEvent, TreeModel} from "ng2-tree";
+
+const nginx_proxy_url = "http://47.95.207.79:41900";
 
 @Component({
-  selector: 'app-document',
-  templateUrl: './document.component.html',
-  styleUrls: ['./document.component.scss']
+    selector: 'app-document',
+    templateUrl: './document.component.html',
+    styleUrls: ['./document.component.scss']
 })
 export class DocumentComponent implements OnInit {
 
-  constructor() { }
+    markdownUrl = nginx_proxy_url + "/valor-software/ng2-tree/master/README.md";
 
-  ngOnInit() {
-  }
+    preBuyDoc: TreeModel = {
+        value: "购前须知",
+        id: "/valor-software/ng2-tree/master/README.md",
+        children: [
+            {
+                id: "/valor-software/ng2-tree/master/README.md",
+                value: "关于HCloud"
+            }
+        ]
+    };
 
+    openVersionDoc: TreeModel = {
+        value: "开源版",
+        id: "/valor-software/ng2-tree/master/README.md",
+        children: [
+            {
+                id: "/valor-software/ng2-tree/master/README.md",
+                value: "关于HCloud"
+            }
+        ]
+    };
+
+    priceVersionDoc: TreeModel = {
+        id: "/houshuai0816/hcloud/dev_hcloud/document/DOC-1-DeployProject.md?token=AEOPFLFJOT5CALCENWZYYVC4WMVKM",
+        value: "付费版",
+        children: [
+            {
+                id: "/houshuai0816/hcloud/dev_hcloud/document/DOC-1-DeployProject.md?token=AEOPFLFJOT5CALCENWZYYVC4WMVKM",
+                value: "关于HCloud"
+            }
+        ]
+    };
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+    handleSelected(e: NodeEvent) {
+        this.markdownUrl = nginx_proxy_url + e.node.id;
+    }
 }
