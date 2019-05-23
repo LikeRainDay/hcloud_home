578b456fbbd61206b140a807383c952f423beb76
houshuai <houshuai>
2019-05-23 23:21:40 +0800

    增加 用户必须登录的守护进程

--- src/app/@core/auth/auth.guard.ts
+++ src/app/@core/auth/auth.guard.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
 import {UserService} from '../service/user.service';
+import {Observable} from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
@@ -10,8 +11,8 @@ export class AuthGuard implements CanActivate {
     constructor(private router: Router, private userService: UserService) {
     }
 
-    // 路由守卫
-    canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
+        Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
         return this.checkLogin();
     }
 
